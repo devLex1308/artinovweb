2d2e955373fea9370de2ce87ef77c912a501f953
Alex <oleksandr.pavliuk1308@gmail.com>
2017-04-29 19:42:57 +0300

    bootstrap start

---
+++ l12/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bootstrap</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+
+
+
+</head>
+<body>
+	<h1>Bootstrap</h1>
+	<div class="container division">
+		<div class="row">
+			<div class="col-md-2 col-sm-6 col"></div>
+			<div class="col-md-5 col-sm-6 col"></div>
+			<div class="col-md-3 col-sm-4 col"></div>
+			<div class="col-md-2 col-sm-4 col"></div>
+			<div class="col-md-2 col-sm-4 col"></div>
+		</div>
+		<div class="row">
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		  <div class="col-md-1">.col-md-1</div>
+		</div>
+	</div>
+</body>
+</html>
