25809f4f0036f057604d07a8b57f0aa8501dd9b1
Alex <oleksandr.pavliuk1308@gmail.com>
2017-05-01 19:17:01 +0300

    l13

---
+++ l13/editUsers.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+			<meta charset="UTF-8">
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<script src="js/main.js"></script>
+			<title>
+					Реєстрація
+			</title>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<a href="index.html">Головна</a>
+			</nav>
+		</header>
+		<div class="content">
+			
+		</div>
+		<footer>
+			
+		</footer>		
+	</body>
+</html>
