878f2278e5f3832d58b342bf02b80b8eed0f745b
Max <dems.ua@gmail.com>
2017-05-06 20:46:39 +0300

    error message

--- l14/views/nav.php
+++
@@ -1,9 +0,0 @@
-<nav>
-	<a href="/l13/">Головна</a>
-	<a href="">Реєстрація</a>
-	<a href="authorization.php">Авторизація</a>
-	<a href="createStation.php">Створення зупинки</a>
-	<a href="editStation.php">Редагування зупинки</a>
-	<a href="">Видалення зупинки</a>
-	<a href="editUsers.php">Редагування юзера</a>
-</nav>
