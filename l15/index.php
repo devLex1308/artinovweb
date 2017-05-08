35b5b13a94c36f7c849f389ca44720096863d98d
Alex <oleksandr.pavliuk1308@gmail.com>
2017-05-08 19:08:17 +0300

    Start l15

---
+++ l15/index.php
@@ -0,0 +1,16 @@
+﻿<?php
+?>
+	
+			<?php 
+				define('ROOT', dirname(__FILE__));
+				define('LOCALPATH',"/l15");
+				include_once "classes/Router.php";
+				$router = new Router();
+				$router->run();
+
+
+			?>
+
+<?php
+
+?>
