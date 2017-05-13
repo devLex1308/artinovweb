430bd0cbbbf7214d5b915636dfbbfde6bc674120
Max <dems.ua@gmail.com>
2017-05-13 12:08:19 +0300

    l14 fies

---
+++ l14/views/nav.php
@@ -0,0 +1,9 @@
+<nav>
+	<a href="/l13/">Головна</a>
+	<a href="">Реєстрація</a>
+	<a href="authorization.php">Авторизація</a>
+	<a href="createStation.php">Створення зупинки</a>
+	<a href="editStation.php">Редагування зупинки</a>
+	<a href="">Видалення зупинки</a>
+	<a href="editUsers.php">Редагування юзера</a>
+</nav>
