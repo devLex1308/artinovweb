83f21cf024aadc15d8645e6318359572b5344963
Alex <oleksandr.pavliuk1308@gmail.com>
2017-05-13 19:59:27 +0300

    add l16, add pagination

---
+++ l16/views/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<meta charset="UTF-8">
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<script src="js/main.js"></script>
+			<title>
+					Реєстрація
+			</title>
+	</head>
+	<body>
+		<header>
+			<?php
+				require("nav.php");
+			?>
+		</header>
