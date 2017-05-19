fe9be40e5d195a23caa72302761bbd8e13b09f86
Denis <Troshuk@gmail.com>
2017-05-20 02:01:05 +0300

    +

--- l17/config/routers.php
+++ l17/config/routers.php
@@ -6,11 +6,13 @@ return array(
 	'admin/station/edit/([0-9]+)' => 'AdminStation/edit/$1',
 	'admin/station/delete/([0-9]+)' => 'AdminStation/delete/$1',
 	'admin/station/([0-9]+)' => 'AdminStation/index/$1',
+	'admin/station' => 'AdminStation/index',
 	
 	'admin/route/create' => 'AdminRoute/create',
 	'admin/route/edit/([0-9]+)' => 'AdminRoute/edit/$1',
 	'admin/route/delete/([0-9]+)' => 'AdminRoute/delete/$1',
 	'admin/route/([0-9]+)' => 'AdminRoute/index/$1',
+	'admin/route' => 'AdminRoute/index',
 
 	'admin/category/create' => 'AdminCategory/create',
 	'admin/category/edit/([0-9]+)' => 'AdminCategory/edit/$1',
