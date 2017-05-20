8ec2134e4ed39507ac97875aefcc0b5345dbf310
Alex <oleksandr.pavliuk1308@gmail.com>
2017-05-20 21:32:30 +0300

    point move

--- l18a/js/main.js
+++ l18a/js/main.js
@@ -3,6 +3,11 @@ var KEY_TOP = 38;
 var KEY_RIGHT = 39;
 var KEY_DOWN = 40;
 
+var POINT_STATUS_MOVE_TOP = 0;
+var POINT_STATUS_MOVE_RIGHT = 1;
+var POINT_STATUS_MOVE_DOWN = 2;
+var POINT_STATUS_MOVE_LEFT = 3;
+
 $(document).ready(function () {
 	var displayWidth = $("#display").data("width");
 	var displayHeight = $("#display").data("height");
@@ -13,29 +18,28 @@ $(document).ready(function () {
 	// var y = 5;
 	var point1 = new Point(10,3);
 
-	// setInterval(function(){
-	// 	point1.down();
-	// 	point1.left();
-	// },1000);
+	setInterval(function(){
+		point1.update();
+	},1000);
 
 	$("body").keydown(function(e){
 
     switch((e.keyCode ? e.keyCode : e.which)){
 
     	case KEY_LEFT:
-    		point1.left();
+    		point1.setPointStatus(POINT_STATUS_MOVE_LEFT);
     		console.log("Клавіша вліво");
     		break;
     	case KEY_TOP:
-    		point1.top();
+    		point1.setPointStatus(POINT_STATUS_MOVE_TOP);
     		console.log("Клавіша вверх");
     		break;
     	case KEY_RIGHT:
-    		point1.right();
+    		point1.setPointStatus(POINT_STATUS_MOVE_RIGHT);
     		console.log("Клавіша вправо");
     		break;
     	case KEY_DOWN:
-    		point1.down();
+    		point1.setPointStatus(POINT_STATUS_MOVE_DOWN);
     		console.log("Клавіша вниз");
     		break;
         default:
@@ -50,9 +54,11 @@ $(document).ready(function () {
 function Point(x,y){
 	var _x;
 	var _y;
+	var _pointStatus;
 	this.init = function(x,y){
 		_x = x;
 		_y = y;
+		_pointStatus = POINT_STATUS_MOVE_RIGHT;
 		this.showPoint();
 	}
 
@@ -76,11 +82,32 @@ function Point(x,y){
 		_x++;
 		this.showPoint();
 	}
+	this.setPointStatus = function(status){
+		_pointStatus = status;
+	}
 	this.showPoint = function(){
 		$("#position_"+_x+"_"+_y).addClass("point");
 	}
 	this.removePoint = function(){
 		$("#position_"+_x+"_"+_y).removeClass("point");
 	}
+
+	this.update = function(){
+		switch(_pointStatus){
+			case POINT_STATUS_MOVE_TOP: 
+				this.top();
+				break;
+			case POINT_STATUS_MOVE_RIGHT: 
+				this.right();
+				break;
+			case POINT_STATUS_MOVE_DOWN: 
+				this.down();
+				break;
+			case POINT_STATUS_MOVE_LEFT: 
+				this.left();
+			break;
+		}
+	}
+
 	this.init(x,y);
 }
