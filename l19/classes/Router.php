17ab78885f4af86636f57488d2ca0db502c98c88
Max <dems.ua@gmail.com>
2017-05-27 01:29:34 +0300

    мої костилі

--- l19/classes/Router.php
+++ l19/classes/Router.php
@@ -24,7 +24,7 @@ class Router
 
 				array_shift($segments);
 				array_shift($segments);
-				// array_shift($segments);
+				array_shift($segments);
 				
 				$controllerName = array_shift($segments).'Controller';
 				$controllerName = ucfirst($controllerName);
@@ -38,7 +38,7 @@ class Router
 				if (file_exists($controllerFile)) {
 					include_once($controllerFile);
 				}
-				
+
 				$controllerObject = new $controllerName;
 				$result = call_user_func_array(array($controllerObject, $actionName), $parameters);
 				
