364a1e35c032c29bcc5826488fb087062bf420fe
Lex <oleksandr.pavliuk1308@gmail.com>
2017-05-26 11:43:52 +0300

    Додав в редагуванні транспорту додавання і редагування часу відправлення

--- l19/controllers/AdminTransportController.php
+++ l19/controllers/AdminTransportController.php
@@ -43,6 +43,44 @@ class AdminTransportController{
 										$_POST['route_id']
 									);
 		}
+		if(isset($_POST["createTimeStart"])){
+			switch($_POST["createTimeStart"]){
+				case "timeStartDirect":
+					$insert_id = TimeRouteStart::createTimeRouteStart($id,true,false);
+					//echo "Створити час відправки за прямим марштутом $insert_id ";
+					break;
+
+				case "timeStartRevert":
+					$insert_id = TimeRouteStart::createTimeRouteStart($id,false,false);
+					//echo "Створити час відправки за зворотнім маршрутом $insert_id";
+					break;
+
+				case "timeStartDirectRest":
+					$insert_id = TimeRouteStart::createTimeRouteStart($id,true,true);
+					//echo "Створити час відправки за прямим марштутом вихідний день $insert_id";
+					break;
+
+				case "timeStartRevertRest":
+					$insert_id = TimeRouteStart::createTimeRouteStart($id,false,true);
+					//echo "Створити час відправки за зворотнім маршрутом вихідний день $insert_id";
+					break;
+				default:
+					break;
+			}
+		}
+
+		if(isset($_POST["action"])&&$_POST["action"]=="saveTimeRouteStart"){
+			if(isset($_POST['time_start'])){
+				foreach ($_POST['time_start'] as $key => $oneRow){
+					echo "Слід відредагувати запис в базі time_route_start з id ={$key} та записати туди значення  $oneRow<br>";
+				}
+			}
+		}
+
+		$aTimeStartDirect = TimeRouteStart::getTimeRouteStart($id,true,false);
+		$aTimeStartRevert = TimeRouteStart::getTimeRouteStart($id,false,false);
+		$aTimeStartDirectRest = TimeRouteStart::getTimeRouteStart($id,true,true);
+		$aTimeStartRevertRest = TimeRouteStart::getTimeRouteStart($id,false,true);
 
 		$title = "Редагування транспорту";
 		require_once ROOT."/views/admin/AdminTransportEdit.php";
--- l19/views/admin/AdminTransportEdit.php
+++ l19/views/admin/AdminTransportEdit.php
@@ -1,6 +1,28 @@
 <?php
 require_once ROOT."/views/admin/header.php";
+function inputTimeRouteStart($array,$id_name){
+    ?>
+    <form class="form-horizontal" role="form" method="post" id="<?php echo $id_name; ?>">
+        <div class="time">
+    <?php
+    foreach ($array as $TimeStartDirect) {
+        echo "<span class = 'inputTimeRouteStart'>";
+            echo "<input type='time' name='time_start[{$TimeStartDirect['id']}]' value='{$TimeStartDirect['time_start']}'>";
+            echo "<a href='".LOCALPATH."/admin/timeroutestart/edit/{$TimeStartDirect['id']}''>Редагувати</a>";
+            echo "<a href='".LOCALPATH."/admin/timeroutestart/edit/{$TimeStartDirect['id']}''>Видалити</a>";
+        echo "</span>";
+    }?>
+        </div>
+        <button type="submit" name="action" value="saveTimeRouteStart" class="btn btn-warning">Зберегти</button>
+    </form>
+    <form class="form-horizontal" role="form" method="post">
+        <button name="createTimeStart" type="submit" value="<?php echo $id_name; ?>" id="create<?php echo ucfirst($id_name);?>" class="btn btn-warning">Додати відправлення</button>
+    </form>
+    <?php
+}
+
 ?>
+    <a href=></a>
 <h1><?php echo $title; echo " ".$transport['name'] ?></h1>
 <div class="container">
     <div class="row">
@@ -55,6 +77,23 @@ require_once ROOT."/views/admin/header.php";
                     </div>
                 </div>
             </form>
+            <div class="col-xs-10 col-sm-10 col-md-8 col-lg-6">
+                <h2 class="text-center">Час відправлення за прямим маршрутом</h2>
+                <?php inputTimeRouteStart($aTimeStartDirect,"timeStartDirect"); ?>
+            </div>
+            <div class="col-xs-10 col-sm-10 col-md-8 col-lg-6">
+                <h2 class="text-center">Час відправлення за зворотнім маршрутом</h2>
+                <?php inputTimeRouteStart($aTimeStartRevert,"timeStartRevert"); ?>
+            </div>
+            <div class="col-xs-10 col-sm-10 col-md-8 col-lg-6">
+                <h2 class="text-center">Час відправлення за прямим маршрутом у вихідний</h2>
+                <?php inputTimeRouteStart($aTimeStartDirectRest,"timeStartDirectRest"); ?>
+            </div>
+            <div class="col-xs-10 col-sm-10 col-md-8 col-lg-6">
+                <h2 class="text-center">Час відправлення за зворотнім маршрутом у вихідний</h2>
+                <?php inputTimeRouteStart($aTimeStartRevertRest,"timeStartRevertRest"); ?>
+            </div>
+
         </div>
         <div class="col-xs-1 col-sm-1 col-md-2 col-lg-3"></div>
     </div>
