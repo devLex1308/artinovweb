17f29ba52e6df456c3f85e4cd03eba6b975e3b93
Alex <oleksandr.pavliuk1308@gmail.com>
2017-05-29 20:46:29 +0300

    slider finish

--- l21a/index.php
+++ l21a/index.php
@@ -23,24 +23,51 @@
 			.slider img{
 				position: absolute;
 				width: 100%;
+				z-index: 1;
+				transition: left 1s linear;
 			}
+
+			.slider img.active{
+				z-index: 2;
+				left: 0;
+			}
+
+			.slider img.activeNext{
+				z-index: 2;
+				left: -100%;
+			}
+			.slider img.activePrev{
+				z-index: 2;
+				left: 100%;
+			}
+			
+			
+			
+	
+
 		</style>
 		<div id="content">
 			<h1>Слайдер</h1>
 
 			<div class="slider">
-				<img src="images/1.jpg" alt="">
-				<img src="images/2.jpg" alt="">
-				<img src="images/3.jpg" alt="">
-				<img src="images/4.jpg" alt="">
-				<img src="images/5.jpg" alt="">
-				<img src="images/6.jpg" alt="">
-				<img src="images/7.jpg" alt="">
-				<img src="images/8.jpg" alt="">
-				<img src="images/9.jpg" alt="">
-				<img src="images/10.jpg" alt="">
+				<div class="wrepImages">	
+					<img src="images/1.jpg" alt="">
+					<img src="images/2.jpg" alt="">
+					<img src="images/3.jpg" alt="">
+					<img src="images/4.jpg" alt="">
+					<img src="images/5.jpg" alt="">
+					<img src="images/6.jpg" alt="">
+					<img src="images/7.jpg" alt="">
+					<img src="images/8.jpg" alt="">
+					<img src="images/9.jpg" alt="">
+					<img src="images/10.jpg" alt="">
+				</div>
+				<div class="wrepButton">	
+					<button class="prev">Назад</button>
+					<button class="next">Вперід</button>
+				</div>
 			</div>
-		
+				
 		</div>
 		<footer>
 			
