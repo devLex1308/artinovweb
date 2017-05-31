2e8edcb664b9f9d9bac485351b8f0ec9af9f34fc
Denis <Troshuk@gmail.com>
2017-05-31 15:00:07 +0300

    delete

--- l21/Створити папку/controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class IndexController {
-	
-	public function actionIndex(){
-		$title = "Головна";
-		require_once ROOT."/views/index.php";
-		return true;
-	}
-}
--- l21/Створити папку/views/admin/footer.php
+++
@@ -1,6 +0,0 @@
-    <script src="<?php echo LOCALPATH;?>/template/menu/js/mdb.js"></script>
-    <script src="<?php echo LOCALPATH;?>/template/menu/js/tether.js"></script>
-    <script src="<?php echo LOCALPATH;?>/template/menu/js/bootstrap.js"></script>
-    <script src="<?php echo LOCALPATH;?>/template/js/tools.js"></script>
-  </body>
-</html>
